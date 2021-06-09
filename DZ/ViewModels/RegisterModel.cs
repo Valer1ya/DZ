6d83044228933dc1e8f9d923b1014ecaa83a2313
Valer1ya <lera.grigoreva@list.ru>
2021-06-09 14:56:34 +0500

    3

--- DZ/ViewModels/RegisterModel.cs
+++ DZ/ViewModels/RegisterModel.cs
@@ -5,8 +5,14 @@ namespace DZ.ViewModels
     public class RegisterModel
     {
         [Required(ErrorMessage = "Не указан ID пользователя или указан неверно")]
+        public string Name { get; set; }
+        public string Surname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
+        [Display(Name = "Запомнить?")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
     }
 }
